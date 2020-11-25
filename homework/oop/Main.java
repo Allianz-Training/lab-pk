e16fa4622dc51ee75073bab4a6e7088f5ed7080d
pk <parkershock@gmail.com>
2020-11-25 16:22:54 +0700

    make it readable

--- homework/oop/Main.java
+++ homework/oop/Main.java
@@ -2,42 +2,53 @@ package homework.oop;
 
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("========================== OOP HW5-6 =========================");
 		Circle circle = new Circle();
 		Circle circle2 = new Circle(2.5);
 		
 		System.out.println(circle);
-		System.out.println(circle.getArea());
-		System.out.println(circle.getCircumference());
+		System.out.println("Circle area = " + circle.getArea());
+		System.out.println("Circle circumference = " + circle.getCircumference());
 		System.out.println(circle2);
-		System.out.println(circle2.getArea());
-		System.out.println(circle2.getCircumference());
+		System.out.println("Circle area = " +circle2.getArea());
+		System.out.println("Circle circumference = " + circle2.getCircumference());
 		
+		System.out.println("\n========================== OOP HW7 =========================");
 		Rectangle rectangle = new Rectangle();
 		Rectangle rectangle2 = new Rectangle(5, 8);
 		System.out.println(rectangle);
-		System.out.println(rectangle.getArea());
-		System.out.println(rectangle.getPerimeter());
+		System.out.println("Rectangle area = " + rectangle.getArea());
+		System.out.println("Rectangle perimeter = " + rectangle.getPerimeter());
 		System.out.println(rectangle2);
-		System.out.println(rectangle2.getArea());
-		System.out.println(rectangle2.getPerimeter());
+		System.out.println("Rectangle area = " + rectangle2.getArea());
+		System.out.println("Rectangle perimeter = " +rectangle2.getPerimeter());
 		
+		System.out.println("\n========================== OOP HW8 =========================");
 		Employee employee = new Employee(1, "John", "Doe", 10000);
 		System.out.println(employee);
-		System.out.println(employee.getAnnualSalary());
-		System.out.println(employee.raiseSalary(50));
+		System.out.println("Annual salary = " + employee.getAnnualSalary());
+		System.out.println("Salary after raise = " + employee.raiseSalary(50));
 		
+		System.out.println("\n========================== OOP2 HW1 =========================");
 		// OOP2 homework1
 		MovableCircle moveableCircle = new MovableCircle(1, 1, 5, 5, 8);
+		System.out.println(moveableCircle);
 		moveableCircle.moveDown();
 		System.out.println(moveableCircle);
+		
+		System.out.println("\n========================== OOP2 HW2 =========================");
 		// OOP2 homework2
 		MovableRectangle movableRectangle = new MovableRectangle(1, 1, 2, 2, 6, 6);
 		System.out.println(movableRectangle);
+		movableRectangle.moveRight();
+		System.out.println(movableRectangle);
+		
+		System.out.println("\n========================== OOP2 HW3 =========================");
 		// OOP2 homework3
 		DiscountRate discountRate = new DiscountRate();
-		System.out.println(discountRate.getServiceDiscountRate("Premium"));
-		System.out.println(discountRate.getServiceDiscountRate("Gold"));
-		System.out.println(discountRate.getServiceDiscountRate("Silver"));
+		System.out.println("Premium service discount: " + discountRate.getServiceDiscountRate("Premium"));
+		System.out.println("Gold service discount: " + discountRate.getServiceDiscountRate("Gold"));
+		System.out.println("Silver service discount: " + discountRate.getServiceDiscountRate("Silver"));
 		
 		Visit visitPremium = new Visit("PK", null);
 		visitPremium.getCustomer().setMember(true);
@@ -45,7 +56,7 @@ public class Main {
 		visitPremium.setServiceExpense(150);
 		visitPremium.setProductExpense(100);
 		System.out.println(visitPremium);
-		System.out.println(visitPremium.getTotalExpense());
+		System.out.println("Total expense = " + visitPremium.getTotalExpense());
 		
 		Visit visitGold = new Visit("Time", null);
 		visitGold.getCustomer().setMember(true);
@@ -53,7 +64,7 @@ public class Main {
 		visitGold.setServiceExpense(150);
 		visitGold.setProductExpense(100);
 		System.out.println(visitGold);
-		System.out.println(visitGold.getTotalExpense());
+		System.out.println("Total expense = " + visitGold.getTotalExpense());
 		
 		Visit visitSilver = new Visit("KP", null);
 		visitSilver.getCustomer().setMember(true);
@@ -61,13 +72,13 @@ public class Main {
 		visitSilver.setServiceExpense(150);
 		visitSilver.setProductExpense(100);
 		System.out.println(visitSilver);
-		System.out.println(visitSilver.getTotalExpense());
+		System.out.println("Total expense = " + visitSilver.getTotalExpense());
 
 		Visit visiter = new Visit("emiT", null);
 		visiter.setServiceExpense(150);
 		visiter.setProductExpense(100);
 		System.out.println(visiter);		
-		System.out.println(visiter.getTotalExpense());
+		System.out.println("Total expense = " + visiter.getTotalExpense());
 
 	}
 }
